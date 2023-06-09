ce49d18e3c11d0e7b470e6eafd10d923bcdda347
ljo1 <me@liam-obrien.uk>
2023-06-09 22:25:40 +0100

    modified:   Version_2.1_Mini/run.py

--- Version_2.1_Mini/run.py
+++ Version_2.1_Mini/run.py
@@ -3,14 +3,11 @@ import webbrowser
 import os
 
 # Install required packages from requirements.txt
-subprocess.check_call(["pip3", "install", "-r", "requirements.txt"])
+subprocess.check_call(["pip", "install", "-r", "requirements.txt"])
 
 
 # Run your first script
-subprocess.run(['python3', 'ScraperTool_AirBnB.py'])
+subprocess.run(['python', 'ScraperTool_AirBnB.py'])
 
 # Once the first script finishes, run your second script
-subprocess.run(['python3', 'ScrapeToMap3.py'])
-
-# Open the file in the default web browser
-webbrowser.open('file://' + file_path)
+subprocess.run(['python', 'ScrapeToMap3.py'])
