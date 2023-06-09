b3ca37c20b49bd04c850c6f796ce09f1e5cc65c1
ljo1 <me@liam-obrien.uk>
2023-06-09 22:32:21 +0100

    modified:   Version_2.1_Mini/run.py

--- Version_2.1_Mini/run.py
+++ Version_2.1_Mini/run.py
@@ -7,7 +7,7 @@ subprocess.check_call(["pip", "install", "-r", "requirements.txt"])
 
 
 # Run your first script
-subprocess.run(['python', 'ScraperTool_AirBnB.py'])
+subprocess.call(['python', 'ScraperTool_AirBnB.py'])
 
 # Once the first script finishes, run your second script
-subprocess.run(['python', 'ScrapeToMap3.py'])
+subprocess.call(['python', 'ScrapeToMap3.py'])
